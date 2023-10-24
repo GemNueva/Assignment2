d8797a6691ef1ab71ecb44902609ababa1fafc17
gemnueva <nuegb001@mymail.unisa.edu.au>
2023-10-24 15:01:29 +1030

    Create CountriesListSearchComponent

---
+++ Assig2/a2reactapp/src/components/CountriesListSearch.js
@@ -0,0 +1,21 @@
+﻿const CountriesListSearch = ({ }) => {
+    return (
+
+        <div>
+            <h2 className="text-center">
+                Component: CountriesListSearch
+            </h2>
+            <hr />
+
+            <p>
+                Body: baodsjnqon jdnsadojn jnaso
+            </p>
+
+
+        </div>
+
+    );
+}
+
+
+export default CountriesListSearch;
