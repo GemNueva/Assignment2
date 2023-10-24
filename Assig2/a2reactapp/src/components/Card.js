41ab43a3c4747ad9d4b10bfb77f831cb0fb60163
gemnueva <nuegb001@mymail.unisa.edu.au>
2023-10-24 16:49:37 +1030

    CARD COMPONENT

---
+++ Assig2/a2reactapp/src/components/Card.js
@@ -0,0 +1,25 @@
+﻿import { Link } from "react-router-dom";
+
+
+const Card = ({ regionId, regionName, ImageUrl }) => (
+
+
+	<div className="card col-4 mb-2" style={{ width: 18 + 'rem' }} >
+
+		<img src={ImageUrl} className="card-img-top" alt={"Image of" + regionName} />
+
+		<div className="card-body">
+
+			<h5 className="card-title">{regionName}</h5>
+
+			<p className="card-text">{regionId}</p>
+
+			<Link to={"/CountriesList/" + regionId} className="btn btn-primary"> View Countries</Link>
+		</div>
+
+	</div>
+
+)
+
+
+export default Card;
