d2e79d39ea1ac8f8b45038c3340e5fc28b5b07d4
gemnueva <nuegb001@mymail.unisa.edu.au>
2023-10-24 13:32:43 +1030

    Imports
    importing router stuff
    
    importing components we use

--- Assig2/a2reactapp/src/index.js
+++ Assig2/a2reactapp/src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./components/Home";
+import Regions from "./components/Regions";
+import CountriesListSearch from "./components/CountriesListSearch";
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
