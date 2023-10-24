f204738d01226b80d5525952859d84e86199ff2a
gemnueva <nuegb001@mymail.unisa.edu.au>
2023-10-24 15:01:42 +1030

    Create Regions Component

---
+++ Assig2/a2reactapp/src/components/Regions.js
@@ -0,0 +1,22 @@
+﻿const Regions = ({}) => {
+    return (
+
+        <div>
+            <h2 className="text-center">
+                Component: Regions
+            </h2>
+            <hr />
+
+            <p>
+                All Regions
+
+            </p>
+
+
+        </div>
+
+    );
+}
+
+
+export default Regions;
