19471675ce221b6893eee780b9a75b3dfef49ddf
gemnueva <nuegb001@mymail.unisa.edu.au>
2023-10-24 16:49:23 +1030

    REGIONLIST
    FETCH DATA THEN MAP TO CARD

---
+++ Assig2/a2reactapp/src/components/RegionList.js
@@ -0,0 +1,40 @@
+﻿import { useState, useEffect } from 'react';
+import Card from './Card.js';
+
+const RegionList = ({ }) => {
+
+    const [regionData, setState] = useState([]);
+
+    useEffect(() =>
+    {
+        fetch("http://localhost:5256/api/A_Regions")
+            .then(response => response.json())
+            .then(data => setState(data))
+            .catch(err =>
+            {
+                console.log(err);
+            });
+    }, [])
+
+    return
+    (
+        <div className="row">
+            {regionData.map((obj) => (
+                <Card
+                    key = {obj.regionId}
+                    regionId = { obj.regionId }
+                    regionName = { obj.regionName }
+                    imageUrl = { obj.imageUrl }
+
+                    />
+                )
+               )
+
+            }
+
+        </div>
+    )
+}
+
+
+export default RegionList;
