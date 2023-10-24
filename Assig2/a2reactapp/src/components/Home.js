3619289abb6f20eef8876597bed773ae8d1e856f
gemnueva <nuegb001@mymail.unisa.edu.au>
2023-10-24 15:01:13 +1030

    Create Home Component

---
+++ Assig2/a2reactapp/src/components/Home.js
@@ -0,0 +1,22 @@
+﻿const Home = ({ }) => {
+    return (
+
+        <div>
+            <h2 className="text-center">
+                Component: Home
+            </h2>
+            <hr />
+
+            <p>
+                Body: baodsjnqon jdnsadojn jnaso
+
+            </p>
+
+
+        </div>
+
+    );
+}
+
+
+export default Home;
