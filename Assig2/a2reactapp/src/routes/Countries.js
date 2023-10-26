3e3b146c24476f3a57d77dd423d74df409a4ddca
gemnueva <nuegb001@mymail.unisa.edu.au>
2023-10-27 02:06:47 +1030

    Countries Route

---
+++ Assig2/a2reactapp/src/routes/Countries.js
@@ -0,0 +1,19 @@
+﻿
+
+const Countries = ({ }) => {
+    return (
+
+        <div>
+            <h2 className="text-center">
+                Route: Countries
+            </h2>
+            <hr />
+
+          
+        </div>
+
+    );
+}
+
+
+export default Countries;
