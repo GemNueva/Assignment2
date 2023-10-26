d6b8341d41e6eabc5bb7b192a7d4ee042e717265
gemnueva <nuegb001@mymail.unisa.edu.au>
2023-10-27 02:06:59 +1030

    Region Route

---
+++ Assig2/a2reactapp/src/routes/Region.js
@@ -0,0 +1,20 @@
+﻿import RegionList from "../components/RegionList";
+
+const Region = () => {
+    return (
+        <div>
+            <h2 className="text-center">
+                Route: Region
+                <br></br>
+                Component: Region List
+            </h2>
+            <hr />
+
+            <RegionList/>
+        </div>
+
+    );
+}
+
+
+export default Region;
