ce02247ec172661b6643892652a450afd613a378
gemnueva <nuegb001@mymail.unisa.edu.au>
2023-10-27 02:09:08 +1030

    Added CSS
    api issues & restarted react app

--- Assig2/a2reactapp/src/components/RegionList.js
+++ Assig2/a2reactapp/src/components/RegionList.js
@@ -1,40 +1,50 @@
 ﻿import { useState, useEffect } from 'react';
 import Card from './Card.js';
 
-const RegionList = ({ }) => {
 
+//RegionList component fetches region data from an API when it's first mounted.
+//For each item in the returned data, it renders a Card component
+//The fetch only works once due to the empty array passed to useEffect.
+
+const RegionList = () => {
+
+    // initialise the state
     const [regionData, setState] = useState([]);
 
-    useEffect(() =>
-    {
+    useEffect(() => {
         fetch("http://localhost:5256/api/A_Regions")
             .then(response => response.json())
             .then(data => setState(data))
-            .catch(err =>
-            {
+            .catch(err => {
                 console.log(err);
-            });
+            })
     }, [])
 
-    return
-    (
-        <div className="row">
-            {regionData.map((obj) => (
-                <Card
-                    key = {obj.regionId}
-                    regionId = { obj.regionId }
-                    regionName = { obj.regionName }
-                    imageUrl = { obj.imageUrl }
+    const cardContainerStyle = {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-between',
+        gap: '20px'
+    }
 
+
+    return (
+
+        
+        <div>            
+            <div className="card-container" style={cardContainerStyle}>
+                {regionData.map((obj) => (
+                    <Card
+                        key={obj.regionId}
+                        regionId={obj.regionId}
+                        regionName={obj.regionName}
+                        imageUrl={obj.imageUrl}
+                        countryCount={obj.countryCount}
                     />
-                )
-               )
-
-            }
-
+                ))}
+            </div>
         </div>
     )
 }
-
-
 export default RegionList;
+
