555e8bc1cf186357529ba320b51fede8e24dc6cf
gemnueva <nuegb001@mymail.unisa.edu.au>
2023-10-27 02:10:30 +1030

    Updated Routes
    Routes:
    Home
    Region
    Countries
    
    Components:
    Card
    RegionList
    CountriesListSearch

--- Assig2/a2reactapp/src/App.js
+++ Assig2/a2reactapp/src/App.js
@@ -3,8 +3,7 @@ import './App.css';
 import { Link, Outlet } from "react-router-dom"; // IMPORT LINK & OUTLET
 
 
-import RegionList from './components/RegionList';
-import CountriesListSearch from './components/CountriesListSearch';
+
 
 
 
@@ -14,13 +13,16 @@ function App() {
         <div className="App Container">
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
+
                     <Link className="navbar-brand" to="/" > Single Page Web Application</Link>
 
                     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                         <div className="navbar-nav">
-                            <Link className="nav-link" to="/Home">Home</Link>
-                            <Link className="nav-link" to="/RegionList">Region List</Link>
-                            <Link className="nav-link" to="/CountriesListSearch">Countries</Link>
+
+                            {/*Link these to Routes*/}
+                            <Link className="nav-link" to="/Home"> Home </Link>
+                            <Link className="nav-link" to="/Region"> Region </Link>
+                            <Link className="nav-link" to="/Countries"> Countries</Link>
 
                         </div>
                     </div>
