5f319bd3f78af54e1af3417c2559fb1bc84be8ac
gemnueva <nuegb001@mymail.unisa.edu.au>
2023-10-30 23:58:58 +1030

    Create a city card

---
+++ Assig2/a2reactapp/src/components/CityCard.js
@@ -0,0 +1,18 @@
+﻿import './Card.css';
+
+const CityCard = ({ cityID, cityName, airQualityYearRange, recordCount }) => {
+
+    return (
+        <div className="card-body">
+            <h5 className="card-title">City Id:{cityID}</h5>
+
+            <p className="card-text">City Name: {cityName}</p>
+
+            <p className="card-text">Air Quality Year Range:{airQualityYearRange}</p>
+
+            <p className="card-text">Record Count:{recordCount}</p>
+        </div>
+    );
+}
+
+export default CityCard;
