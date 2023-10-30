12a09ad953eed79111109428f791d6b9a552d9e6
gemnueva <nuegb001@mymail.unisa.edu.au>
2023-10-30 16:29:07 +1030

    check if fields are null

--- Assig2/a2reactapp/src/components/CountryCard.js
+++ Assig2/a2reactapp/src/components/CountryCard.js
@@ -6,13 +6,13 @@ const CountryCard = ({ countryId, countryName, imageUrl, iso3, cityCount, emissi
     return (
         <div className="card-body">
 
-            <img src={imageUrl} className="card-img-top" alt={"Image of" + countryName} />
+            <img src={imageUrl} className="card-img-top" alt={countryName}/>
 
             <h5 className="card-title">Country Name:{countryName}</h5>
             <p className="card-text"> Region Id: {countryId}</p>
-            <p className="card-text"> ISO3: {iso3}</p>
-            <p className="card-text"> City Count:{cityCount}</p>
-            <p className="card-text"> Temperature Data Year Range:{temperatureDataYearRange}</p>
+            <p className="card-text"> ISO3: {iso3 ?? "N/A"}</p>
+            <p className="card-text"> City Count:{cityCount ?? "N/A"}</p>
+            <p className="card-text"> Temp Data Year Range:{temperatureDataYearRange}</p>
             <p className="card-text"> Emission Data Year Range:{emissionDataYearRange}</p>
 
 
