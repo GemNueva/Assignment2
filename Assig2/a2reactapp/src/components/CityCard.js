039316778c5b5235d52ef3e2d60a809b50764088
gemnueva <nuegb001@mymail.unisa.edu.au>
2023-10-31 00:40:01 +1030

    Link to CityAQD

--- Assig2/a2reactapp/src/components/CityCard.js
+++ Assig2/a2reactapp/src/components/CityCard.js
@@ -1,4 +1,5 @@
 ﻿import './Card.css';
+import { Link } from 'react-router-dom';
 
 const CityCard = ({ cityID, cityName, airQualityYearRange, recordCount }) => {
 
@@ -11,6 +12,14 @@ const CityCard = ({ cityID, cityName, airQualityYearRange, recordCount }) => {
             <p className="card-text">Air Quality Year Range:{airQualityYearRange}</p>
 
             <p className="card-text">Record Count:{recordCount}</p>
+
+            {/*Link to CitiesList*/}
+            {recordCount ?
+                <Link to={"/CityAQD/" + cityID}> View AQD </Link>
+                : <p> No Air Quality Data</p>
+
+            }
+
         </div>
     );
 }
