fdc660d425db3d19565865e5f4f79e03ff9187c0
gemnueva <nuegb001@mymail.unisa.edu.au>
2023-11-14 15:50:58 +1030

    City AQD Card

---
+++ Assig2/a2reactapp/src/components/CityAQDCard.js
@@ -0,0 +1,22 @@
+﻿import './Card.css';
+import { Link } from 'react-router-dom';
+
+const CityAQDCard = ({ cityName, cityID, countryName, countryId, imageUrl, iso3, regionName, regionId }) => {
+
+    return (
+        <div className="card-body">
+            <p className="card-text">City Name: {cityName}</p>
+            <img src={imageUrl} className="card-img-top" alt={"Image of" + cityName} />
+            <h5 className="card-title">City Id:{cityID}</h5>
+            
+            <p className="card-text">Country Name: {countryName}</p>
+            <p className="card-text">Country Name: {countryId}</p>
+            <p className="card-text">ISO3:{iso3}</p>
+            <p className="card-text">Region Name: {regionName}</p>
+            <p className="card-text">Region Id: {regionId}</p>
+
+        </div>
+    );
+}
+
+export default CityAQDCard;
