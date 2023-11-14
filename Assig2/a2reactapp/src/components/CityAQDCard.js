98a36a99c94b25fb06a5d7122abe55fe50d021d8
gemnueva <nuegb001@mymail.unisa.edu.au>
2023-11-14 21:56:05 +1030

    template literals

--- Assig2/a2reactapp/src/components/CityAQDCard.js
+++ Assig2/a2reactapp/src/components/CityAQDCard.js
@@ -6,7 +6,9 @@ const CityAQDCard = ({ cityName, cityID, countryName, countryId, imageUrl, iso3,
     return (
         <div className="card-body">
             <p className="card-text">City Name: {cityName}</p>
-            <img src={imageUrl} className="card-img-top" alt={"Image of" + cityName} />
+
+            <img src={imageUrl} className="card-img-top" alt={`Image of: ${cityName}`} />
+
             <h5 className="card-title">City Id:{cityID}</h5>
             
             <p className="card-text">Country Name: {countryName}</p>
